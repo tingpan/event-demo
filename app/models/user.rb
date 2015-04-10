619bd97512da87b06a2593f4b5de70d8c2a2a582
tingpan <tingpan666@gmail.com>
2015-04-10 05:52:08 +0100

    add Bootstrap gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,4 @@ class User < ActiveRecord::Base
   belongs_to :team
   has_many :accesses,           :dependent => :destroy
   has_many :projects,           :through => :accesses
-
-  validates :name,              :presence: true
 end
