0e75feb09f5ab4cced2257bec4076278fd1f8487
tingpan <tingpan666@gmail.com>
2015-04-11 12:17:44 +0800

    change the selector

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # event_index_demo
 A simple timeline app in rails
+
+###How to use
+```
+$ bundle install
+$ rake db:setup
+```
--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,9 @@
 class EventsController < ApplicationController
   def index
+    @title = "Event controller demo"
     @user = User.first
+    @team = @user.team
+    @teammates = User.where("team_id = (?)", @team)
     @projects = @user.projects
     accessable_project = @projects.map { |project| project[:id] }
     @events = Event.where("project_id in (?)", accessable_project).limit(50).order(created_at: :desc)
